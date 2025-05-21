8f93e4451d755ddd2b717ecaca3c95ad38e6a45c
Anton Slavin <slavinant@gmail.com>
2025-05-21 18:29:40 +0300

    Add some data

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
-const names = ['Jaak', 'Maria', 'Mirjam', 'Jaan', 'Abkhad', 'Valdar', 
-               'Vladlen', 'Lilita', 'Joonas', 'Krista', 'Marina', 'Robert',
+const names = ['Jaak', 'Maria', 'Mirjam', 'Jaan', 'Abkhad', 'Valdar', 'Aljona', 'Max', 'Semjon',
+               'Vladlen', 'Lilita', 'Joonas', 'Krista', 'Marina', 'Robert', 'Georg',
                'Andrew', 'Ulrich', 'Henri', 'Nina', 'Ella', 'Elen'];
 
 const conds = [null, 
     ['Stroke', 'No limbs', 'Heart explosion', 'Smashed brain'], 
     ['Ebola', 'Necrosis', 'Lung shot'], 
-    ['Blood urination', 'Swollen eye', 'Missing nose'], 
-    ['Fever', 'Diarrhea (violent)', 'Heart palpilations', 'Skin damage'], 
+    ['Blood urination', 'Swollen eye', 'Missing nose', 'Intense mania'], 
+    ['Fever', 'Diarrhea (violent)', 'Heart palpilations', 'Skin damage', 'Mania (light)'], 
     ['Tummy ache', 'Sore throat', 'Slight fever', 'Leg pain', 'Pain', 'Diarrhea', 'Gas']
   ];
